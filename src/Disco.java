06dcb8486b1e408440dfc596c2967557f04fac91
William Cesar Santos <william_cesar_santos@hotmail.com>
2024-02-02 19:39:35 -0300

    Update

---
+++ src/Disco.java
@@ -0,0 +1,15 @@
+public class Disco {
+
+    //Atributos ou propriedades descrevem as caracteristicas de um objeto
+    public String nome;
+    public String autor;
+    public String genero;
+    public Integer anoLancamento;
+    public Integer numeroFaixas;
+
+    //Acoes são descritas como métodos
+    public String fornecerDados() {
+        return "la la la la la la la la";
+    }
+
+}
---
+++ src/MainDisco.java
@@ -0,0 +1,13 @@
+public class MainDisco {
+
+    public static void main(String[] args) {
+        Disco disco = new Disco();
+        disco.anoLancamento = 1988;
+        disco.autor = "Toddy Oziborne";
+        disco.nome = "Iron man";
+        disco.genero = "Rock/Metal";
+        disco.numeroFaixas = 15;
+        disco.fornecerDados();
+    }
+
+}
